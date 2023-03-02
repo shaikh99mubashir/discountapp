75a8c2287cb6f2c73791e5d1fda01d1ec36a41f0
DeskWorkSold <deskworksoldevs@gmail.com>
2023-03-02 15:55:48 +0500

    LoginUpdate

--- src/Screens/Login/index.js
+++ src/Screens/Login/index.js
@@ -7,6 +7,10 @@ import app from "../../FirebaseConfig/Config";
 import { getDatabase, ref, onValue } from "firebase/database";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { Col, Container, Image, Row } from "react-bootstrap";
+// import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 
 const Login = () => {
     const [email, setEmail] = useState("");
@@ -33,18 +37,59 @@ const Login = () => {
                 .catch((error) => {
                     const errorCode = error.code;
                     const errorMessage = error.message;
-                    alert('error==>', errorCode)
+                    alert('error==>', errorCode, errorMessage)
                 });
         });
     };
 
 
     return (
-        <div>
-            <input type='email' onChange={(e) => setEmail(e.target.value)} />
-            <input type='password' onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={loginBtnClicked}>Login</button>
-        </div>
+        <Container fluid style={{ background: "#ecf0f3" }}>
+            <div className="main">
+                <Container className="b-container">
+                    <div>
+                        <Row>
+                            <Col>
+                                <div className="switch">
+                                    <div className="switch__circle"></div>
+                                    <div className="switch__circle switch__circle--t"></div>
+                                    <div className="switch__container" id="switch-c1">
+                                        <h2 className="switch__title title">Welcome Back !</h2>
+                                        <p className="switch__description description">To keep connected with us please login with your personal info</p>
+                                        <button className="switch__button button switch-btn">SIGN IN</button>
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col>
+                                <h2 class="form_title title">Sign in to Website</h2>
+                                <div class="form__icons"></div>
+                                <span class="form__span">or use your email account</span>
+
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <input className="form__input" type='email' onChange={(e) => setEmail(e.target.value)} />
+                                </Form.Group>
+
+                                <Form.Group className="mb-3" controlId="formBasicPassword">
+                                    <input className="form__input" type='password' onChange={(e) => setPassword(e.target.value)} />
+
+                                </Form.Group>
+
+                                <button className="form__button button" onClick={loginBtnClicked}>SIGN IN</button>
+
+                            </Col>
+                        </Row>
+
+                    </div>
+
+                </Container>
+            </div>
+            {/* <div>
+                <input type='email' onChange={(e) => setEmail(e.target.value)} />
+                <input type='password' onChange={(e) => setPassword(e.target.value)} />
+                <button onClick={loginBtnClicked}>Login</button>
+            </div> */}
+        </Container>
+
     )
 }
 
