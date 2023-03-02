07f3f1be5a8957f819399e8c7ab8d18215fc74c7
DeskWorkSold <deskworksoldevs@gmail.com>
2023-03-02 14:19:56 +0500

    update

--- src/Screens/UploadVideo/index.js
+++ src/Screens/UploadVideo/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 // import app from '../../../FirebaseConfig/Config';
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import {
@@ -22,12 +22,28 @@ const UploadVideo = () => {
     const auth = getAuth(app);
     const db = getDatabase(app);
 
+
+    const [videoData, setVideoData] = useState([])
+    React.useEffect(() => {
+        const getData = dbRef(db, `videolink/`);
+        onValue(getData, (e) => {
+            const val = e.val()
+            const data = Object.entries(val).map(([key, value]) => {
+                return {
+                    ...value,
+                    id: key,
+                };
+            });
+            setVideoData(data);
+        });
+    }, []);
+    console.log('videoData', videoData);
+
     const [uploadVideo, setUploadVideo] = useState({
         videoLink: '',
         startDate: '',
         endDate: ''
     })
-    console.log('startDate', uploadVideo.startDate);
 
     console.log('uploadVideo', uploadVideo);
 
@@ -58,19 +74,90 @@ const UploadVideo = () => {
         })
 
     }
-    const date = new Date();
-    let currentTime = date.getTime()
+    // useEffect(() => {
+    //     videoData.some((scheduledVideo) => {
+    //         let startDate = new Date(scheduledVideo.startDate)
+    //         let endDate = new Date(scheduledVideo.endDate)
+    //         console.log(startDate, "startDate======>")
+    //         console.log('endDate=======>', endDate);
+    //         let startDayDifference = startDate.getDate()
+    //         let startMonthDifference = startDate.getMonth()
+    //         let startYear = startDate.getFullYear()
+    //         let startHour = startDate.getHours()
+    //         let startMin = startDate.getMinutes()
 
-    let currentDay = date.getDate()
-    let currentMonth = date.getMonth()
-    let currentYear = date.getFullYear()
-    const currenthours = Math.floor(currentTime / 3600000);
-    const currentminutes = Math.floor((currentTime % 3600000) / 60000);
-    const currentseconds = Math.floor(((currentTime % 3600000) % 60000) / 1000);
+    //         let userSelectedStartDay = uploadVideo.startDate.getDate()
+    //         let userSelectedStartMonth = uploadVideo.startDate.getMonth()
+    //         let userSelectedStartYear = uploadVideo.startDate.getFullYear()
+    //         let userSelectedStartHour = uploadVideo.startDate.getHours()
+    //         let userSelectedStartMin = uploadVideo.startDate.getMinutes()
 
+    //         let endDayDifference = endDate.getDate()
+    //         let endMonthDifference = endDate.getMonth()
+    //         let endYear = endDate.getFullYear()
+    //         let endHour = endDate.getHours()
+    //         let endMin = endDate.getMinutes()
+
+    //         let userSelectedEndDay   = uploadVideo.endDate.getDate()
+    //         let userSelectedEndMonth = uploadVideo.endDate.getMonth()
+    //         let userSelectuserSelectedEndMonthedEndYear  = uploadVideo.endDate.getFullYear()
+    //         let userSelectedEndHour  = uploadVideo.endDate.getHours()
+    //         let userSelectedEndMin  = uploadVideo.endDate.getMinutes()
+
+
+
+    //         console.log('uploadVideo.startDate', uploadVideo.startDate);
+    //         console.log('uploadVideo.startDate', uploadVideo.endDate);
+    //         console.log('scheduledVideo', scheduledVideo);
+    //         const startTime = new Date(scheduledVideo.startDate).getTime();
+    //         const endTime = new Date(scheduledVideo.endDate).getTime();
+    //         // console.log('startYime', startTime);
+    //         return uploadVideo.startDate >= startTime && uploadVideo.startDate <= endTime;
+    //     });
+    // }, [uploadVideo.startDate])
 
     const sendVideo = () => {
-        if (uploadVideo.videoLink == "" || uploadVideo.startDate == "" || uploadVideo.endDate == "") {
+
+        const isOverlap = videoData.length > 0 && videoData.every((scheduledVideo) => {
+            let startDate = new Date(scheduledVideo.startDate)
+            let endDate = new Date(scheduledVideo.endDate)
+            let userStartDate = new Date(uploadVideo?.startDate)
+            let userEndDate = new Date(uploadVideo.endDate)
+            console.log(startDate, "startDate======>")
+            console.log('endDate=======>', endDate);
+            let startDayDifference = startDate.getDate()
+            let startMonthDifference = startDate.getMonth()
+            let startYear = startDate.getFullYear()
+            let startHour = startDate.getHours()
+            let startMin = startDate.getMinutes()
+
+            let userSelectedStartDay = userStartDate.getDate()
+            let userSelectedStartMonth = userStartDate.getMonth()
+            let userSelectedStartYear = userStartDate.getFullYear()
+            let userSelectedStartHour = userStartDate.getHours()
+            let userSelectedStartMin = userStartDate.getMinutes()
+
+            let endDayDifference = endDate.getDate()
+            let endMonthDifference = endDate.getMonth()
+            let endYear = endDate.getFullYear()
+            let endHour = endDate.getHours()
+            let endMin = endDate.getMinutes()
+
+            let userSelectedEndDay = userEndDate.getDate()
+            let userSelectedEndMonth = userEndDate.getMonth()
+            let userSelectuserEndYear = userEndDate.getFullYear()
+            let userSelectedEndHour = userEndDate.getHours()
+            let userSelectedEndMin = userEndDate.getMinutes()
+            return userStartDate >= startDate && userStartDate <= endDate
+        });
+
+        console.log(isOverlap, 'isOverlapisOverlapisOverlapisOverlap');
+
+        if (isOverlap) {
+
+            alert("The selected time overlaps with an existing scheduled video.");
+        }
+        else if (uploadVideo.videoLink == "" || uploadVideo.startDate == "" || uploadVideo.endDate == "") {
             alert('Kindly Fill The Fields')
         }
         else {
@@ -86,11 +173,12 @@ const UploadVideo = () => {
         }
     }
 
+
     return (
         <>
             <div>UploadVideo</div>
             <input type="file" accept="video/mp4" onChange={handleFileInputChange} />
-            <button type="submit" onClick={handleSubmit} disabled={!selectedFile}>{selectedFile ? 'Upload Successfully' : 'Upload'}</button>
+            <button type="submit" onClick={handleSubmit} disabled={!selectedFile}>{uploadVideo.videoLink ? 'Upload Successfully' : 'Upload'}</button>
             <br />
             <label for=""> Vedio Start Date And Time</label>
 
