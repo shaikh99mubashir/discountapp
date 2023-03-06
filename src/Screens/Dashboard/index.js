741c50da9f6eff8503212da72195166063a39278
mubashir odho <mubashirodho@yahoo.com>
2023-03-06 16:26:22 +0500

    commit

--- src/Screens/Dashboard/index.js
+++ src/Screens/Dashboard/index.js
@@ -1,36 +1,215 @@
-import React from 'react'
-import Slidebar from '../Slidebar'
-import { Link, useLocation } from 'react-router-dom'
-import { Row, Col, Container } from 'react-bootstrap'
-import { RxDashboard } from 'react-icons/rx';
+import React from "react";
+import Slidebar from "../Slidebar";
+import { Link, useLocation } from "react-router-dom";
+import { Row, Col, Container } from "react-bootstrap";
+import {
+  AiOutlineBars,
+  AiFillSchedule,
+  AiOutlineOrderedList,
+  AiFillPushpin,
+  AiFillSetting,
+  BiSupport,
+} from "react-icons/ai";
+import { MdOutlineContactSupport } from "react-icons/md";
+
+import { RxDashboard } from "react-icons/rx";
+
 const Dashboard = () => {
+  const location = useLocation();
+  console.log(location, "location");
+  const { state } = location;
 
-    const location = useLocation()
-    console.log(location, "location")
-    const { state } = location
+  return (
+    <>
+      <Slidebar
+        title="Dashboard"
+        style={{ color: "#d47617", fontSize: 30, fontWeight: "bold" }}
+      />
+      <Container>
+        <Row>
+          <Col lg="3">
+            <div className="box-shad">
+              <Link to="/Dashboard" style={{ textDecoration: "none" }}>
+                <div>
+                  <button
+                    className="sliderButton"
+                    style={{
+                      width: "100%",
+                      display: "flex",
+                      alignItems: "flex-start",
+                      alignItems: "center",
+                      gap: 10,
+                      borderRadius: 5,
+                    }}
+                  >
+                    {" "}
+                    <RxDashboard id="sliderIcon" color="white" size={30} />
+                    <span style={{ fontSize: 20 }}>Dashboard</span>{" "}
+                  </button>
+                </div>
+              </Link>
+            </div>
+          </Col>
+          <Col lg="3">
+            <div className="box-shad">
+              <Link to="/UploadVideo" style={{ textDecoration: "none" }}>
+                <div>
+                  <button
+                    className="sliderButton"
+                    style={{
+                      width: "100%",
+                      display: "flex",
+                      alignItems: "flex-start",
+                      alignItems: "center",
+                      gap: 10,
+                      borderRadius: 5,
+                    }}
+                  >
+                    {" "}
+                    <AiFillSchedule id="sliderIcon" color="white" size={30} />
+                    <span style={{ fontSize: 20 }}>
+                      Scheduled New Video
+                    </span>{" "}
+                  </button>
+                </div>
+              </Link>
+            </div>
+          </Col>
+          <Col lg="3">
+            <div className="box-shad">
+              <Link to="/ScheduledVideoList" style={{ textDecoration: "none" }}>
+                <div>
+                  <button
+                    className="sliderButton"
+                    style={{
+                      width: "100%",
+                      display: "flex",
+                      alignItems: "flex-start",
+                      alignItems: "center",
+                      gap: 10,
+                      borderRadius: 5,
+                    }}
+                  >
+                    {" "}
+                    <AiOutlineOrderedList
+                      id="sliderIcon"
+                      color="white"
+                      size={30}
+                    />
+                    <span style={{ fontSize: 20 }}>Scheduled Video List</span>{" "}
+                  </button>
+                </div>
+              </Link>
+            </div>
+          </Col>
+          <Col lg="3">
+            <div className="box-shad">
+              <Link to="/PushNotification" style={{ textDecoration: "none" }}>
+                <div style={{ textDecoration: "none" }}>
+                  <div>
+                    <button
+                      className="sliderButton"
+                      style={{
+                        width: "100%",
+                        display: "flex",
+                        alignItems: "flex-start",
+                        alignItems: "center",
+                        gap: 10,
+                        borderRadius: 5,
+                      }}
+                    >
+                      {" "}
+                      <AiFillPushpin id="sliderIcon" color="white" size={20} />
+                      <span style={{ fontSize: 20 }}>
+                        Push Notification
+                      </span>{" "}
+                    </button>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          </Col>
+          <Col lg="3">
+            <div className="box-shad">
+              <Link to="/Setting" style={{ textDecoration: "none" }}>
+                <div>
+                  <button
+                    className="sliderButton"
+                    style={{
+                      width: "100%",
+                      display: "flex",
+                      alignItems: "flex-start",
+                      alignItems: "center",
+                      gap: 10,
+                      borderRadius: 5,
+                    }}
+                  >
+                    {" "}
+                    <AiFillSetting id="sliderIcon" color="white" size={20} />
+                    <span style={{ fontSize: 20 }}>Setting</span>{" "}
+                  </button>
+                </div>
+              </Link>
+            </div>
+          </Col>
+          <Col lg="3">
+            <div className="box-shad">
+              <Link to="/Support" style={{ textDecoration: "none" }}>
+                <div>
+                  <button
+                    className="sliderButton"
+                    style={{
+                      width: "100%",
+                      display: "flex",
+                      alignItems: "flex-start",
+                      alignItems: "center",
+                      gap: 10,
+                      borderRadius: 5,
+                    }}
+                  >
+                    {" "}
+                    <MdOutlineContactSupport
+                      id="sliderIcon"
+                      color="white"
+                      size={20}
+                    />
+                    <span style={{ fontSize: 20 }}>Support</span>{" "}
+                  </button>
+                </div>
+              </Link>
+            </div>
+          </Col>
+          <Col lg="3">
+            <div className="box-shad">
+              <Link to="/Marquee" style={{ textDecoration: "none" }}>
+                <div>
+                  <button
+                    className="sliderButton"
+                    style={{
+                      width: "100%",
+                      display: "flex",
+                      alignItems: "flex-start",
+                      alignItems: "center",
+                      gap: 10,
+                      borderRadius: 5,
+                    }}
+                  >
+                    {" "}
+                    <MdOutlineContactSupport
+                      id="sliderIcon"
+                      color="white"
+                      size={20}
+                    />
+                    <span style={{ fontSize: 20 }}>Marquee</span>{" "}
+                  </button>
+                </div>
+              </Link>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
 
-    return (
-        <>
-            <Slidebar title='Dashboard' style={{ color: "#d47617", fontSize: 30, fontWeight: 'bold' }} />
-            <Container>
-                <Row style={{ gap: 20 }}>
-                    <Col lg="2" style={{ border: "1px solid #d47617", height: '100px', backgroundColor: "#d47617", display: 'flex', justifyContent: "center", alignItems: "center" }}>
-                        <Link to='/Dashboard' style={{ textDecoration: 'none' }}>
-                            <div style={{ marginBottom: 20 }}>
-                                <button className='sliderButton' style={{ width: '100%', display: 'flex', alignItems: 'flex-start', alignItems: 'center', gap: 10, borderRadius: 5 }}> <RxDashboard id='sliderIcon' color='white' size={20} /><span style={{ fontSize: 20 }}>Dashboard</span> </button>
-
-                            </div>
-                        </Link>
-                    </Col>
-                    <Col lg="2" style={{ border: "1px solid #d47617", height: '150px', backgroundColor: "#d47617" }}></Col>
-                    <Col lg="2" style={{ border: "1px solid #d47617", height: '150px', backgroundColor: "#d47617" }}></Col>
-                    <Col lg="2" style={{ border: "1px solid #d47617", height: '150px', backgroundColor: "#d47617" }}></Col>
-                    <Col lg="2" style={{ border: "1px solid #d47617", height: '150px', backgroundColor: "#d47617" }}></Col>
-                    <Col lg="2" style={{ border: "1px solid #d47617", height: '150px', backgroundColor: "#d47617" }}></Col>
-                </Row>
-            </Container>
-        </>
-    )
-}
-
-export default Dashboard
+export default Dashboard;
--- src/Screens/UploadVideo/index.js
+++ src/Screens/UploadVideo/index.js
@@ -1,197 +1,238 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 // import app from '../../../FirebaseConfig/Config';
-import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-import {
-    getAuth,
-    onAuthStateChanged,
-} from "firebase/auth";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import app from "../../FirebaseConfig/Config";
-import ReactPlayer from 'react-player'
+import ReactPlayer from "react-player";
 import {
-    getDatabase,
-    onValue,
-    ref as dbRef,
-    push,
-    set,
-    update,
-    remove,
+  getDatabase,
+  onValue,
+  ref as dbRef,
+  push,
+  set,
+  update,
+  remove,
 } from "firebase/database";
 // import Button from 'react-bootstrap';
-import { Form, Table, Button } from 'react-bootstrap';
-import Slidebar from '../Slidebar';
-
+import { Form, Table, Button, Container } from "react-bootstrap";
+import Slidebar from "../Slidebar";
+import Loader from "../../Images/loading.gif";
 const UploadVideo = () => {
-    const [selectedFile, setSelectedFile] = useState(null);
-    const [uploadProgress, setUploadProgress] = useState(0);
-    const auth = getAuth(app);
-    const db = getDatabase(app);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const auth = getAuth(app);
+  const db = getDatabase(app);
 
+  const [videoData, setVideoData] = useState([]);
+  React.useEffect(() => {
+    const getData = dbRef(db, `videolink/`);
+    onValue(getData, (e) => {
+      const val = e.val();
+      const data = Object.entries(val).map(([key, value]) => {
+        return {
+          ...value,
+          id: key,
+        };
+      });
+      setVideoData(data);
+    });
+  }, []);
 
-    const [videoData, setVideoData] = useState([])
-    React.useEffect(() => {
-        const getData = dbRef(db, `videolink/`);
-        onValue(getData, (e) => {
-            const val = e.val()
-            const data = Object.entries(val).map(([key, value]) => {
-                return {
-                    ...value,
-                    id: key,
-                };
-            });
-            setVideoData(data);
-        });
-    }, []);
+  const [uploadVideo, setUploadVideo] = useState({
+    videoName: "",
+    videoLink: "",
+    startDate: "",
+    endDate: "",
+  });
 
-    const [uploadVideo, setUploadVideo] = useState({
-        videoName: '',
-        videoLink: '',
-        startDate: '',
-        endDate: ''
-    })
+  console.log("uploadVideo", uploadVideo);
 
-    console.log('uploadVideo', uploadVideo);
-
-    function handleFileInputChange(event) {
-        const file = event.target.files[0];
-        if (file && file.type === 'video/mp4') {
-            setSelectedFile(file);
-        } else {
-            setSelectedFile(null);
-        }
+  function handleFileInputChange(event) {
+    const file = event.target.files[0];
+    if (file && file.type === "video/mp4") {
+      setSelectedFile(file);
+    } else {
+      setSelectedFile(null);
     }
+  }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        // Create a storage reference
-        const storage = getStorage();
-        const storageRef = ref(storage, 'videos/' + selectedFile.name);
-        // Upload the file to Firebase Storage
-        const uploadTask = uploadBytes(storageRef, selectedFile);
-        console.log('upload', uploadTask);
-        uploadTask.then((snapshot) => {
-            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            setUploadProgress(progress);
-            getDownloadURL(storageRef).then((downloadURL) => {
-                setUploadVideo({ ...uploadVideo, videoLink: downloadURL })
-                // console.log('File available at', downloadURL);
-            });
+  function handleSubmit(event) {
+    event.preventDefault();
+    // Create a storage reference
+    const storage = getStorage();
+    const storageRef = ref(storage, "videos/" + selectedFile.name);
+    // Upload the file to Firebase Storage
+    const uploadTask = uploadBytes(storageRef, selectedFile);
+    console.log("upload", uploadTask);
+    uploadTask.then((snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setUploadProgress(progress);
+      getDownloadURL(storageRef).then((downloadURL) => {
+        setUploadVideo({ ...uploadVideo, videoLink: downloadURL });
+        // console.log('File available at', downloadURL);
+      });
+    });
+  }
+
+  const sendVideo = () => {
+    const isOverlap =
+      videoData.length > 0 &&
+      videoData.every((scheduledVideo) => {
+        let startDate = new Date(scheduledVideo.startDate);
+        let endDate = new Date(scheduledVideo.endDate);
+        let userStartDate = new Date(uploadVideo?.startDate);
+        let userEndDate = new Date(uploadVideo.endDate);
+        let startTime = userStartDate.getTime();
+        let endTime = endDate.getTime();
+        return startTime < endTime;
+      });
+
+    console.log(isOverlap, "isOverlapisOverlapisOverlapisOverlap");
+
+    if (isOverlap) {
+      alert("The selected time overlaps with an existing scheduled video.");
+    } else if (
+      uploadVideo.videoLink == "" ||
+      uploadVideo.startDate == "" ||
+      uploadVideo.endDate == ""
+    ) {
+      alert("Kindly Fill The Fields");
+    } else {
+      const reference = dbRef(db, `videolink/`);
+      push(reference, uploadVideo)
+        .then(() => {
+          alert("Successfully Scheduled!");
+          uploadVideo.videoName = "";
+          uploadVideo.videoLink = "";
+          uploadVideo.startDate = "";
+          uploadVideo.endDate = "";
         })
-
-    }
-
-    const sendVideo = () => {
-
-
-        const isOverlap = videoData.length > 0 && videoData.every((scheduledVideo) => {
-
-            let startDate = new Date(scheduledVideo.startDate)
-            let endDate = new Date(scheduledVideo.endDate)
-            let userStartDate = new Date(uploadVideo?.startDate)
-            let userEndDate = new Date(uploadVideo.endDate)
-            let startTime = userStartDate.getTime()
-            let endTime = endDate.getTime()
-            return startTime < endTime
+        .catch((err) => {
+          console.log(err);
         });
-
-        console.log(isOverlap, 'isOverlapisOverlapisOverlapisOverlap');
-
-        if (isOverlap) {
-
-            alert("The selected time overlaps with an existing scheduled video.");
-        }
-        else if (uploadVideo.videoLink == "" || uploadVideo.startDate == "" || uploadVideo.endDate == "") {
-            alert('Kindly Fill The Fields')
-        }
-        else {
-            const reference = dbRef(db, `videolink/`)
-            push(reference, uploadVideo).then(() => {
-                alert('Successfully Scheduled!')
-                uploadVideo.videoName = ""
-                uploadVideo.videoLink = ""
-                uploadVideo.startDate = ""
-                uploadVideo.endDate = ""
-            }).catch((err) => {
-                console.log(err);
-            })
-        }
     }
+  };
 
+  return (
+    <>
+      <Slidebar
+        title="Schedule Video"
+        style={{ color: "#d47617", fontSize: 30, fontWeight: "bold" }}
+      />
+      <Container>
+        <div class="row align-items-center">
+          <div class="col-lg-6">
+            <div
+              style={{
+                marginTop: 40,
+                border: "1px solid white",
+                borderRadius: 5,
+                padding: 30,
+              }}
+            >
+              <div style={{ color: "#222536" }}>Video Name</div>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <input
+                  className="form-control form__input"
+                  type="email"
+                  placeholder="Video Name"
+                  onChange={(e) =>
+                    setUploadVideo({
+                      ...uploadVideo,
+                      videoName: e.target.value,
+                    })
+                  }
+                />
+              </Form.Group>
+              <div style={{ color: "#222536" }}>UploadVideo</div>
+              <input
+                className="form-control form__input"
+                type="file"
+                accept="video/mp4"
+                onChange={handleFileInputChange}
+              />
+              <button
+                className="button-sub px-4"
+                type="submit"
+                onClick={handleSubmit}
+                disabled={!selectedFile}
+              >
+                {uploadVideo.videoLink ? "Upload Successfully" : "Upload"}
+              </button>
+              <br />
+              <label for="" style={{ color: "#222536" }}>
+                {" "}
+                Vedio Start Date And Time
+              </label>
 
-    return (
-        <>
-            <Slidebar title='Schedule Video' style={{ color: "#d47617", fontSize: 30, fontWeight: 'bold' }} />
-            <div class="row">
-                <div class="col-lg-4">
-                    <div style={{ marginTop: 40, border: '1px solid white', borderRadius: 5, padding: 30 }}>
-                        <div style={{ color: '#222536' }}>Video Name</div>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <input className="form__input" type='email' placeholder="Video Name" onChange={(e) => setUploadVideo({ ...uploadVideo, videoName: e.target.value, })} />
-                        </Form.Group>
-                        <div style={{ color: '#222536', }}>UploadVideo</div>
-                        <input className='form-control form__input' type="file" accept="video/mp4" onChange={handleFileInputChange} />
-                        <button className='button-sub px-4' type="submit" onClick={handleSubmit} disabled={!selectedFile}>{uploadVideo.videoLink ? 'Upload Successfully' : 'Upload'}</button>
-                        <br />
-                        <label for="" style={{ color: '#222536' }}> Vedio Start Date And Time</label>
+              <input
+                className="form-control form__input"
+                style={{ marginTop: "5px" }}
+                type="datetime-local"
+                id="startDateTime"
+                name="Schedule Date And Time"
+                value={uploadVideo.startDate}
+                min={new Date().toISOString().slice(0, 16)}
+                onChange={(e) => {
+                  const selectedDateTime = new Date(e.target.value).getTime();
+                  const currentDateTime = new Date().getTime();
+                  if (selectedDateTime < currentDateTime) {
+                    alert("Please select a current or future date and time.");
+                  } else {
+                    setUploadVideo({
+                      ...uploadVideo,
+                      startDate: e.target.value,
+                    });
+                  }
+                }}
+              />
 
-                        <input
-                            className='form-control form__input'
-                            style={{ marginTop: "5px" }}
-                            type="datetime-local"
-                            id="startDateTime"
-                            name="Schedule Date And Time"
-                            value={uploadVideo.startDate}
-                            min={new Date().toISOString().slice(0, 16)}
-                            onChange={(e) => {
-                                const selectedDateTime = new Date(e.target.value).getTime();
-                                const currentDateTime = new Date().getTime();
-                                if (selectedDateTime < currentDateTime) {
-                                    alert("Please select a current or future date and time.");
-                                } else {
-                                    setUploadVideo({
-                                        ...uploadVideo,
-                                        startDate: e.target.value,
-                                    });
-                                }
-                            }}
-                        />
+              <label for="" style={{ color: "#222536" }}>
+                {" "}
+                Vedio End Date And Time{" "}
+              </label>
+              <input
+                className="form-control form__input"
+                style={{ marginTop: "5px" }}
+                type="datetime-local"
+                id="startDateTime"
+                name="Schedule Date And Time"
+                value={uploadVideo.endDate}
+                min={new Date().toISOString().slice(0, 16)}
+                onChange={(e) => {
+                  const selectedDateTime = new Date(e.target.value).getTime();
+                  const currentDateTime = new Date().getTime();
+                  if (selectedDateTime < currentDateTime) {
+                    alert("Please select a current or future date and time.");
+                  } else {
+                    setUploadVideo({
+                      ...uploadVideo,
+                      endDate: e.target.value,
+                    });
+                  }
+                }}
+              />
 
-                        <label for="" style={{ color: '#222536' }}> Vedio End Date And Time </label>
-                        <input
-                            className='form-control form__input'
-                            style={{ marginTop: "5px" }}
-                            type="datetime-local"
-                            id="startDateTime"
-                            name="Schedule Date And Time"
-                            value={uploadVideo.endDate}
-                            min={new Date().toISOString().slice(0, 16)}
-                            onChange={(e) => {
-                                const selectedDateTime = new Date(e.target.value).getTime();
-                                const currentDateTime = new Date().getTime();
-                                if (selectedDateTime < currentDateTime) {
-                                    alert("Please select a current or future date and time.");
-                                } else {
-                                    setUploadVideo({
-                                        ...uploadVideo,
-                                        endDate: e.target.value,
-                                    });
-                                }
-                            }}
-                        />
-
-                        <br />
-                        <button className="button-sub px-4" type="submit" onClick={sendVideo}>Submit</button>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div style={{ marginTop: 40, border: '1px solid white', borderRadius: 5, padding: 30 }}>
-                        <ReactPlayer controls url={uploadVideo.videoLink} />
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                </div>
+              <br />
+              <button
+                className="button-sub px-4"
+                type="submit"
+                onClick={sendVideo}
+              >
+                Submit
+              </button>
             </div>
-        </>
-    )
-}
+          </div>
+          <div class="col-lg-6">
+            <div className="ved-m">
+              <img src={Loader} style={{ width: 30, height: 30 }} />
+              <ReactPlayer controls url={uploadVideo.videoLink} />
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
 
-export default UploadVideo
+export default UploadVideo;
